0a16ee9390c274966711230c34efd95d7a758f6b
Tien <vucongtien031101@gmail.com>
2022-12-17 16:42:37 +0700

    tien commit

--- views/admin/sidebar.php
+++ views/admin/sidebar.php
@@ -1,6 +1,6 @@
 <div class="sidebar">
     <ul>
-        <li name="trochoi"><a href="?role=admin&page=trochoi"><i class="fa fa-mobile"></i>Quản lý trò chơi</a></li>
+        <li name="trochoi"><a href="?role=admin&page=trochoi"><i class="fa fa-mobile"></i>Quản lý trò chơi abc</a></li>
         <li name="taikhoan"><a href="?role=admin&page=taikhoan"><i class="fa fa-bars"></i>Quản lý tài khoản</a></li>
         <li name="hoadon"><a href="?role=admin&page=hoadon"><i class="fa fa-money"></i>Quản lý đơn hàng</a></li>
         <!-- <li name="binhluan"><a href="?role=admin&page=binhluan"><i class="fa fa-comment"></i>Quản lý bình luận</a></li>
